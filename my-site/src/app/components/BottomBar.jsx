080559241845f4eeef8104ba6faf11418aec143a
Stephen Tao <stephentao04@gmail.com>
2024-10-15 02:41:43 -0400

    fix resume link

--- my-site/src/app/components/BottomBar.jsx
+++ my-site/src/app/components/BottomBar.jsx
@@ -10,7 +10,7 @@ export default function BottomBar(){
     <div className=' mt-8 text-4xl text-frost-1-custom dark:text-snowstorm-1-custom flex justify-center space-x-8 '>
       <a className="hover:text-frost-2 dark:hover:text-snowstorm-1" href='https://www.linkedin.com/in/stephen-tao-7ab814210' target='_blank'><AiFillLinkedin /></a>
       <a className="hover:text-frost-2 dark:hover:text-snowstorm-1"href='https://github.com/stephzilla20' target='_blank'><AiFillGithub /></a>
-      <a className="hover:text-frost-2 dark:hover:text-snowstorm-1" href='https://drive.google.com/file/d/1wlpAVqTCgWQi2DhCj7yvsmTWmBbN77iQ/view?usp=sharing' target="_blank"><IoMdDocument/></a>
+      <a className="hover:text-frost-2 dark:hover:text-snowstorm-1" href='https://drive.google.com/drive/folders/1l4DHbbjp_z07gfQAARV0bWPxk8goKVzo?usp=sharing' target="_blank"><IoMdDocument/></a>
     </div>
 
   )
