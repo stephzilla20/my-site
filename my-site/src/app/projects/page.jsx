2964163722c1e77fbe6d9c4ea26f8e545361e366
Stephen Tao <stephentao04@gmail.com>
2024-10-15 02:27:56 -0400

    fix projects page (finally)

--- my-site/src/app/projects/page.jsx
+++ my-site/src/app/projects/page.jsx
@@ -1,17 +1,21 @@
+"use client"
 import React from 'react'
-import { Card, CardHeader, CardBody, CardFooter, Tag, Text, TagLabel, HStack} from '@chakra-ui/react'
+import { Card, CardHeader,VStack, CardBody, CardFooter, Tag, Text, TagLabel, HStack} from '@chakra-ui/react'
 import { extendTheme } from '@chakra-ui/react'
+import Microlink from '@microlink/react'
+import {AiFillGithub} from 'react-icons/ai';
 
 
 function Projects() {
 
   const projects =[
-    {title: "stephen-tao.com.", source: "https://github.com/stephzilla20", desc: "The site you are on right now! Built with Nextjs and Tailwind. Using Vercel to deploy.", link: "https://stephen-tao.com", tags: ["Nextjs", "Tailwind", "Vercel"]},
-    {title: "Gist", source: "https://github.com/DxctorHu/Deerhacks-III-2024", desc: "", link: "", tags: ["React/Nextjs", "Python/Flask","Open AI"]},
-    {title: "Smoggle Maps", source: "https://github.com/mustafakhafaji/deltahacks-x", desc: "A mobile based app aimed to provide navigation while encourging users to be concious about their CO2 emissions whenA mobile-based app aimed to provide navigation while encouraging users to be conscious of their CO2 emissions when traveling. travelling ", link: "",tags: ["React-Native", "Expo Go","Google Maps API"]},
-    {title: "Stand", source: "https://github.com/stephzilla20/Stand", desc: "", link: "",tags: ["React/Nextjs", "Java/Spring Boot","Postgresql"]}
+    {title: "stephen-tao.com.", source: "https://github.com/stephzilla20", desc: "", link: "https://stephen-tao.com", tags: ["Nextjs", "Tailwind", "Vercel"]},
+    {title: "Gist", source: "https://github.com/DxctorHu/Deerhacks-III-2024", desc: "", link: "https://devpost.com/software/gist-es7d1t", tags: ["React" , "Nextjs", "Python", "Flask","Open AI"]},
+    {title: "Smoggle Maps", source: "https://github.com/mustafakhafaji/deltahacks-x", desc: " ", link: "https://devpost.com/software/smoggle-maps",tags: ["React-Native", "Expo Go","Google Maps API"]},
+    {title: "EcoEats", source: "https://github.com/stephzilla20/terrahacks", desc: "", link: "https://devpost.com/software/ecoeats-6ky3rx",tags: ["React Native","Flask", "Google Gemini", "Azure Vision","Expo Go"]}
   ]
-
+// A mobile based app aimed to provide navigation while encourging users to be concious about their CO2 emissions whenA mobile-based a
+// pp aimed to provide navigation while encouraging users to be conscious of their CO2 emissions when traveling. travelling
   const breakpoints = {
     base: '0px',
     sm: '320px',
@@ -31,27 +35,42 @@ function Projects() {
         {projects.map((project) => (
           <div className='m-4 ' key={project.title}>
             <Card boxSize={{base: '350', md: '300', lg:'450'}}>
-              <CardBody>
                 <CardHeader fontSize={25}>
                   {/* check value of link, if not empty then add as link else just display title */}
                   {project.link ? (
-                    <a href = {project.link} target= "_blank">
-                      {project.title}  
+                    <a 
+                      href = {project.source} 
+                      target= "_blank"
+                      className='hover:underline'
+                    >
+                       <HStack spacing={2}>
+                        <Text>{project.title}</Text>
+                        <AiFillGithub />
+                      </HStack>
                     </a>              
                   ) : (
                   <span>{project.title}</span>
                   )}
                 </CardHeader>
-                <a href = {project.source} target='_blank'>Source</a>
-                  <Text>
+                <CardBody>
+                  {/* <Text
+                    fontSize={{base:14, md:16}}
+                  >
                     {project.desc}
-                  </Text>
-                
+                  </Text> */}
+                  <div key={project.title}>
+                    <Microlink url={project.link}/>
+                  </div>
+                 
+                </CardBody>
                 <CardFooter>
-                 <HStack spacing={'4'}>
+                 <HStack 
+                 spacing={'4'}
+                 flexWrap={'wrap'}
+                 >
                     {project.tags.map((tag) => (  
                         <Tag
-                        size={'md'}
+                        size={{base:'md', md:'lg'}}
                         variant={'subtle'}
                         colorScheme={'blue'}
                       > 
@@ -60,7 +79,6 @@ function Projects() {
                     ))}
                   </HStack>
                 </CardFooter>
-              </CardBody>
             </Card>
           </div>
         ))}
